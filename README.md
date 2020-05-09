c84582f1baa66ac383df77b370fcd9d5271c464f
Pedro Amoedo <pamoedom@redhat.com>
2020-05-10 01:30:42 +0200

    v1.3 (Indirect edges)

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # ocp4upc
 OCP4 Upgrade Paths Checker
 ## Description
-This script generates a graphical output of the possible OpenShift4 **minor upgrade paths** using production-ready **stable** and **fast** [channels](https://docs.openshift.com/container-platform/4.3/updating/updating-cluster-between-minor.html#understanding-upgrade-channels_updating-cluster-between-minor).
+This is just a bash script that generates a graphical output of the possible **minor upgrade paths** using production-ready **stable** and **fast** [OpenShift 4 channels](https://docs.openshift.com/container-platform/4.3/updating/updating-cluster-between-minor.html#understanding-upgrade-channels_updating-cluster-between-minor).
 ## Usage
 ~~~
 $ ./ocp4upc.sh source_version [arch]
 ~~~
 ## Example
 ~~~
-$ ./ocp4upc.sh 4.2.25
+$ ./ocp4upc.sh 4.2.26
 [INFO] Checking prerequisites... [OK] 
-[INFO] Checking if '4.2.25' (amd64) is a valid release... [OK] 
-[INFO] Result exported as 'stable-4.3.svg'
-[INFO] Result exported as 'fast-4.3.svg'
+[INFO] Checking if '4.2.26' (amd64) is a valid release... [OK] 
+[INFO] Result exported as 'fast-4.3_20200510.svg'
+[INFO] Result exported as 'stable-4.3_20200510.svg'
 ~~~
-![fast-4.3 example](https://github.com/pamoedom/ocp4upc/blob/master/examples/fast-4.3.png)
+![fast-4.3 example](https://github.com/pamoedom/ocp4upc/blob/master/examples/fast-4.3_20200510.png)
 ## Dependencies
 - `curl` <https://curl.haxx.se/>
 - `jq` <http://stedolan.github.io/jq/>
--- ocp4upc.sh
+++ ocp4upc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-VERSION="1.2"
+VERSION="1.3"
+BIN="/usr/bin"
 
 #INFO = (
 #          author      => 'Pedro Amoedo'
@@ -10,14 +11,22 @@ VERSION="1.2"
 #       );
 
 #CHANGELOG = (
-#              * v1.0
-#                - Initial commit
-#              * v1.1
-#                - Checking releases against quay API
-#                - Removing skopeo prerequisite
+#              * v1.3
+#                - Fixing strict release parsing for ${POS}
+#                - Enforcing ${BIN} path for all binaries
+#                - ${LTS} for both channels
+#                - Channels order swapped to properly match LTS
+#                - Adding timestamp on exported files
+#                - Colorize all edges reaching ${LTS}
+#                - Some cosmetic changes
 #              * v1.2
 #                - Adding timestamp in the graph title (rfc-3339)
 #                - Fixed prerequisites list (bc)
+#              * v1.1
+#                - Checking releases against quay API
+#                - Removing skopeo prerequisite
+#              * v1.0
+#                - Initial commit
 #            );
 
 #VARs DESCRIPTION
@@ -25,23 +34,23 @@ VERSION="1.2"
 #$2=arch (optional, default = amd64)
 
 #ANSI COLORS
-OK="echo -en \\033[1;32m"
-ERROR="echo -en \\033[1;31m"
-WARN="echo -en \\033[1;33m"
-INFO="echo -en \\033[1;34m"
-NORM="echo -en \\033[0;39m"
+OK="${BIN}/echo -en \\033[1;32m"
+ERROR="${BIN}/echo -en \\033[1;31m"
+WARN="${BIN}/echo -en \\033[1;33m"
+INFO="${BIN}/echo -en \\033[1;34m"
+NORM="${BIN}/echo -en \\033[0;39m"
 
 usage(){
-  echo "OCP4 Upgrade Paths Checker (stable,fast) v${VERSION}"
-  echo "---------------------------------------------"
-  echo "Usage: $0 source_version [arch]"
-  echo "---------------------------------------------"
+  ${BIN}/echo "OCP4 Upgrade Paths Checker (fast,stable) v${VERSION}"
+  ${BIN}/echo "---------------------------------------------"
+  ${BIN}/echo "Usage: $0 source_version [arch]"
+  ${BIN}/echo "---------------------------------------------"
   exit 1
 }
 
 #Pretty Print function ($type_of_msg,$string,$echo_opts)
 cout(){
-  echo -n "[" && eval "\$$1" && echo -n "$1" && $NORM && echo -n "] " && echo $3 "$2"
+  ${BIN}/echo -n "[" && eval "\$$1" && ${BIN}/echo -n "$1" && $NORM && ${BIN}/echo -n "] " && ${BIN}/echo $3 "$2"
 }
 
 #VARIABLES
@@ -49,20 +58,22 @@ cout(){
 GPH='https://api.openshift.com/api/upgrades_info/v1/graph'
 REL='https://quay.io/api/v1/repository/openshift-release-dev/ocp-release'
 VER=$1
-BIN="/usr/bin"
-MAJ=`${BIN}/echo ${VER} | cut -d. -f1`
-MIN=`${BIN}/echo ${VER} | cut -d. -f2`
+MAJ=`${BIN}/echo ${VER} | ${BIN}/cut -d. -f1`
+MIN=`${BIN}/echo ${VER} | ${BIN}/cut -d. -f2`
 TRG="${MAJ}.`${BIN}/echo ${MIN}+1 | ${BIN}/bc`"
 EDG="blue"
+EDGt="red"
 ORG="salmon"
 DST="yellowgreen"
 [[ -z $2 ]] && ARC="amd64" || ARC=$2
-POS=""
+POSv=""
+POSc=""
 PTH="/tmp/${0##*/}"
-CHA=(stable fast)
+CHA=(fast stable) #The order here is relevant to colorize the LTS per channel, see JQ_SCRIPT below.
 REQ=(curl jq dot bc)
-RES=()
-LTS=""
+RES=() #Array of resulting channels in case of discard.
+LTS=() #Array of latest target releases per channel.
+EXT=() #Array of indirect nodes
 
 #PREREQUISITES
 
@@ -98,8 +109,8 @@ cout "OK" ""
 #OBTAIN UPGRADE PATHS JSONs
 for chan in ${CHA[@]}; do ${BIN}/curl -sH 'Accept:application/json' "${GPH}?channel=${chan}-${TRG}&arch=${ARC}" > ${PTH}/${chan}-${TRG}.json; [ $? -ne 0 ] && cout "ERROR" "Unable to curl 'https://${GPH}?channel=${chan}-${TRG}&arch=${ARC}'. Aborting" && exit 1; done
 
-##capture the latest target version within fast channel (TODO: do this against the API directly?)
-LTS=`${BIN}/cat ${PTH}/fast-${TRG}.json | ${BIN}/jq . | ${BIN}/grep "\"${TRG}." | ${BIN}/cut -d'"' -f4 | ${BIN}/sort -urV | ${BIN}/head -1`
+##capture the latest target version within each channel (TODO: do this against the API?)
+for chan in ${CHA[@]}; do LTS=("${LTS[@]}" "$(${BIN}/cat ${PTH}/${chan}-${TRG}.json | ${BIN}/jq . | ${BIN}/grep "\"${TRG}." | ${BIN}/cut -d'"' -f4 | ${BIN}/sort -urV | ${BIN}/head -1)"); done
 
 #JSON to GV
 JQ_SCRIPT='"digraph TITLE {\n  labelloc=t;\n  rankdir=BT;\n  label=CHANNEL" as $header |
@@ -112,7 +123,8 @@ JQ_SCRIPT='"digraph TITLE {\n  labelloc=t;\n  rankdir=BT;\n  label=CHANNEL" as $
                if .value.metadata.url then ",url=\"" + .value.metadata.url + "\"" else "" end
              ) + (
 	       if .value.version == "'"${VER}"'" then ",shape=polygon,sides=5,peripheries=3,style=filled,color='"${ORG}"'"
-	       elif .value.version == "'"${LTS}"'" then ",shape=square,style=filled,color='"${DST}"'"
+	       elif .value.version == "'"${LTS[0]}"'" then ",shape=square,style=filled,color='"${DST}"'"
+	       elif .value.version == "'"${LTS[1]}"'" then ",shape=square,style=filled,color='"${DST}"'"
 	       elif .value.version >= "'"${TRG}"'" then ",shape=square,style=filled,color=lightgrey"
 	       else ",shape=ellipse,style=filled,color=lightgrey"
 	       end
@@ -130,27 +142,51 @@ JQ_SCRIPT='"digraph TITLE {\n  labelloc=t;\n  rankdir=BT;\n  label=CHANNEL" as $
 '
 for chan in ${CHA[@]}; do ${BIN}/jq -r "${JQ_SCRIPT}" ${PTH}/${chan}-${TRG}.json > ${PTH}/${chan}-${TRG}.gv; done
 
-#DISCARD CHANNELS & COLORIZE EDGES
+#DISCARD CHANNELS & COLORIZE EDGES (TODO: move this logic into JQ_SCRIPT?)
 for chan in ${CHA[@]}
 do
-  POS="" && POS=`grep "\"${VER}\"" ${PTH}/${chan}-${TRG}.gv | awk {'print $1'}`
-  if [ "${POS}" = "" ]
+  POSv="" && POSv=`grep "\"${VER}\"" ${PTH}/${chan}-${TRG}.gv | awk {'print $1'}`
+  if [ "${POSv}" = "" ]
     then
-     cout "WARN" "Skipping channel '${chan}-${TRG}', version '${VER}' not found."
+      cout "WARN" "Skipping channel '${chan}-${TRG}', version '${VER}' not found."
     else
-      sed -i -e 's/^\s\s'"${POS}"'\(->.*$\)/  edge \[color='"${EDG}"'\,style=bold];\n  '"${POS}"'\1\ \n  edge \[color=black\,style=solid];/g' ${PTH}/${chan}-${TRG}.gv
+      ##colorize source_version outgoing edges
+      ${BIN}/sed -i -e 's/^\(\s\s'"${POSv}"'->.*\)\;$/\1 [color='"${EDG}"'\,style=bold];/g' ${PTH}/${chan}-${TRG}.gv
+
+      ##capture indirect node edges
+      EXT=($(grep "\s\s${POSv}->" ${PTH}/${chan}-${TRG}.gv | ${BIN}/cut -d">" -f2 | ${BIN}/cut -d";" -f1))
+
+      ##save resulting channels for subsequent operations
       RES=("${RES[@]}" "${chan}")
+
+      ##colorize EXT->LTS edges
+      if [ "${chan}" = "fast" ]
+        then
+          POSc="" && POSc=`grep "\"${LTS[0]}\"" ${PTH}/${chan}-${TRG}.gv | awk {'print $1'}`
+	  [[ "${POSc}" = "" ]] && continue
+          for edge in ${EXT[@]}; do ${BIN}/sed -i -e 's/^\(\s\s'"${edge}"'->'"${POSc}"'.*\)\;$/\1 [color='"${EDG}"'\,style=dashed,label="Indirect"];/g' ${PTH}/${chan}-${TRG}.gv; done
+	  ##put special color to direct LTS edge (if any)
+	  ${BIN}/sed -i -e 's/^\(\s\s'"${POSv}"'->'"${POSc}"'\).*$/\1 [color='"${EDGt}"'\,style=bold,label="Direct"];/g' ${PTH}/${chan}-${TRG}.gv
+      elif [ "${chan}" = "stable" ]
+        then
+          POSc="" && POSc=`grep "\"${LTS[1]}\"" ${PTH}/${chan}-${TRG}.gv | awk {'print $1'}`
+          [[ "${POSc}" = "" ]] && continue
+          for edge in ${EXT[@]}; do ${BIN}/sed -i -e 's/^\(\s\s'"${edge}"'->'"${POSc}"'.*\)\;$/\1 [color='"${EDG}"'\,style=dashed,label="Indirect"];/g' ${PTH}/${chan}-${TRG}.gv; done
+          ##put special color to direct LTS edge (if any)
+          ${BIN}/sed -i -e 's/^\(\s\s'"${POSv}"'->'"${POSc}"'.*\)\;$/\1 [color='"${EDGt}"'\,style=bold,label="Direct"];/g' ${PTH}/${chan}-${TRG}.gv
+      fi
   fi
 done
 
+#abort if the provided release is not present within any of the channels
 [[ ${#RES[@]} -eq 0 ]] && cout "ERROR" "Version '${VER}' not found within any of the ${TRG} channels. Aborting" && exit 1
 
 #LABELING
-for chan in ${RES[@]}; do sed -i -e 's/TITLE/'"${chan}"'/g' ${PTH}/${chan}-${TRG}.gv; done
-for chan in ${RES[@]}; do sed -i -e 's/CHANNEL/"'"${chan}"'-'"${TRG}"' \('"$(${BIN}/date --rfc-3339=date)"'\)"/g' ${PTH}/${chan}-${TRG}.gv; done
+for chan in ${RES[@]}; do ${BIN}/sed -i -e 's/TITLE/'"${chan}"'/g' ${PTH}/${chan}-${TRG}.gv; done
+for chan in ${RES[@]}; do ${BIN}/sed -i -e 's/CHANNEL/"'"${chan}"'-'"${TRG}"' \('"$(${BIN}/date --rfc-3339=date)"'\)"/g' ${PTH}/${chan}-${TRG}.gv; done
 
 #DRAW & EXPORT
-for chan in ${RES[@]}; do ${BIN}/dot -Tsvg ${PTH}/${chan}-${TRG}.gv -o ${chan}-${TRG}.svg; [ $? -ne 0 ] && cout "ERROR" "Unable to export the results. Aborting" && exit 1 || cout "INFO" "Result exported as '${chan}-${TRG}.svg'"; done
+for chan in ${RES[@]}; do ${BIN}/dot -Tsvg ${PTH}/${chan}-${TRG}.gv -o ${chan}-${TRG}_$(date +%Y%m%d).svg; [ $? -ne 0 ] && cout "ERROR" "Unable to export the results. Aborting" && exit 1 || cout "INFO" "Result exported as '${chan}-${TRG}_$(date +%Y%m%d).svg'"; done
 
 #EOF
 exit 0
